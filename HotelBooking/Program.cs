c93fd21fedb99f1b6b385e848ad78a71e63f9165
mig-25 <kd3180@hotmail.com>
2019-10-21 18:53:03 +0200

    Add project files.

---
+++ HotelBooking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelBooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
